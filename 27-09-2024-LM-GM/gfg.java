4f46b5855a80d118f0fe0cf930365e28bfaac60a
tanmayrsm <45991527+tanmayrsm@users.noreply.github.com>
2024-09-27 09:33:31 +0530

    Add files via upload

---
+++ 27-09-2024-LM-GM/gfg.java
@@ -0,0 +1,33 @@
+
+// User function template for JAVA
+class Solution {
+    public ArrayList<Integer> max_of_subarrays(int k, int arr[]) {
+        ArrayList<Integer> result = new ArrayList<>();
+        Deque<Integer> deque = new LinkedList<>();
+        
+        for (int i = 0; i < k; i++) {
+            while (!deque.isEmpty() && arr[i] >= arr[deque.peekLast()]) {
+                deque.removeLast();
+            }
+            deque.addLast(i);
+        }
+        
+        for (int i = k; i < arr.length; i++) {
+            result.add(arr[deque.peek()]);
+            
+            while (!deque.isEmpty() && deque.peek() <= i - k) {
+                deque.removeFirst();
+            }
+            
+            while (!deque.isEmpty() && arr[i] >= arr[deque.peekLast()]) {
+                deque.removeLast();
+            }
+            
+            deque.addLast(i);
+        }
+        
+        result.add(arr[deque.peek()]);
+        
+        return result;
+    }
+}
