ea83e9414303a0c22844e030a33fbd0f93082386
tanmayrsm <45991527+tanmayrsm@users.noreply.github.com>
2025-05-25 11:25:09 +0530

    Add files via upload

---
+++ 25-05-2025-LM-GM/gfg.java
@@ -0,0 +1,24 @@
+class Solution {
+    boolean pythagoreanTriplet(int[] arr) {
+        // code here
+        int n = arr.length;
+        TreeSet<Integer> st = new TreeSet<>();
+        for(int x :  arr)   st.add(x); 
+        List<Integer> st2 = new ArrayList<>(st);
+        
+        for(int i = st2.size() - 1; i >= 2; i--) {
+            int l = 0, r = i - 1;
+            long cSquare = st2.get(i)*st2.get(i);
+            while(l < r) {
+                long aSquare = st2.get(l)*st2.get(l), 
+                bSquare = st2.get(r)*st2.get(r);
+                
+                if( aSquare + bSquare > cSquare)  r--;
+                else if (aSquare + bSquare < cSquare) l++;
+                else if (aSquare + bSquare == cSquare)    return true;
+            }
+        }
+        
+        return false;
+    }
+}
