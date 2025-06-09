9b7ca03dc2405efb24623dee729f9cc20b3a998c
tanmayrsm <45991527+tanmayrsm@users.noreply.github.com>
2025-06-09 09:59:09 +0530

    Add files via upload

---
+++ 09-06-2025-LH-GM/gfg.java
@@ -0,0 +1,26 @@
+class Solution
+{
+    public static boolean isDeadEnd(Node root){
+        //Add your code here.
+        ArrayList<Integer> al = new ArrayList<>();
+        al.add(0);
+        helper(al, root);
+        return sol(al, root);
+    }
+
+    private static void helper(ArrayList<Integer> al, Node root) {
+        if(root==null) return;
+        helper(al, root.left);
+        al.add(root.data);
+        helper(al, root.right);
+        return;
+    }
+
+    private static boolean sol(ArrayList<Integer> al, Node root) {
+        if(root == null) return false;
+        if(root.left == null && root.right == null )
+            if(al.contains(root.data-1) && al.contains(root.data+1))
+                return true;
+        return sol(al, root.left) || sol(al, root.right);
+    }
+}
