c9d50d29c90b983291ac23d1d2079071ddaa2f64
tanmayrsm <45991527+tanmayrsm@users.noreply.github.com>
2025-07-11 11:08:07 +0530

    Add files via upload

---
+++ 11-07-2025-LH-GM/gfg.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int countConsec(int n) {
+        // code here
+        if (n == 2) return 1;
+        int st = 1, fib1 = 0, fib2 = 1;
+        for (int i = 3; i <= n; i++) {
+            st = 2*st + (fib2 + fib1);
+            int temp = fib2;
+            fib2 = fib1 + fib2;
+            fib1 = temp;
+        }
+        return st;
+    }
+}
