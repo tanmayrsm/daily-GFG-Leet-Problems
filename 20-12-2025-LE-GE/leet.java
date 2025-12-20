ab78d21cc033896fcc6e4928b7e9967829ccf06d
tanmayrsm <45991527+tanmayrsm@users.noreply.github.com>
2025-12-20 10:22:21 +0530

    Add files via upload

---
+++ 20-12-2025-LE-GE/leet.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int minDeletionSize(String[] strs) {
+        int ans = 0, n = strs.length, m = strs[0].length();
+        for (int i = 0; i < m; i++) {
+            for (int j = 1; j < n; j++) {
+                if (strs[j].charAt(i) < strs[j - 1].charAt(i)) {
+                    ans++;
+                    break;
+                }
+            }
+        }
+        return ans;
+    }
+}
