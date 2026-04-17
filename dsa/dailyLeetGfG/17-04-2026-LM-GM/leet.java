1b64f576775d7830a839cadf6ffda621a829913b
Tanmay Rishikesh Mishra <tanmayrsm@gmail.com>
2026-04-17 12:07:33 +0530

    chore: daily auto-push 2026-04-17 12:07 IST

--- dsa/dailyLeetGfG/17-04-2026-LM-GM/leet.java
+++ dsa/dailyLeetGfG/17-04-2026-LM-GM/leet.java
@@ -2,17 +2,21 @@ class Solution {
     public int minMirrorPairDistance(int[] nums) {
         Map<Integer, Integer> mp = new HashMap<>();
         int n = nums.length, ans = Integer.MAX_VALUE;
-        for(int i = 0; i < n; i++) {
-            if(mp.containsKey(getRev(nums[i]))) {
-                ans = Math.min(ans, i - mp.get(getRev(nums[i])));
-            }
-            mp.put(nums[i], i);
-        }
+        // for(int i = 0; i < n; i++) {
+        //     int rev = getRev(nums[i]);
+        //     System.out.println(nums[i] + "::" + rev);
+        //     if(mp.containsKey(rev)) {
+        //         ans = Math.min(ans, i - mp.get(rev));
+        //     }
+        //     mp.put(nums[i], i);
+        // }
         mp.clear();
 
         for(int i = n - 1; i >= 0; i--) {
-            if(mp.containsKey(getRev(nums[i]))) {
-                ans = Math.min(ans, mp.get(getRev(nums[i])) - i);
+            int rev = getRev(nums[i]);
+            // System.out.println(nums[i] + "::" + rev);
+            if(mp.containsKey(rev)) {
+                ans = Math.min(ans, mp.get(rev) - i);
             }
             mp.put(nums[i], i);
         }
@@ -23,8 +27,8 @@ class Solution {
     private int getRev(int no) {
         int rev = 0;
         while(no > 0) {
-            rev += no % 10;
             rev *= 10;
+            rev += no % 10;
             no /= 10;
         }
         return rev;
