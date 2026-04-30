f11074d0ed20c8deaa959bc68d5dcd613b91b135
Tanmay Rishikesh Mishra <tanmayrsm@gmail.com>
2026-04-30 12:00:06 +0530

    chore: daily auto-push 2026-04-30 12:00 IST

---
+++ dsa/dailyLeetGfG/30-04-2026-LM-GM/gfg.java
@@ -0,0 +1,11 @@
+class Solution {
+    public boolean isMaxHeap(int[] arr) {
+        // code here
+        int n = arr.length;
+        for(int i=1;i<n;i++){
+            int parent=(i+1)/2;
+            if(arr[parent-1]<arr[i])return false;
+        }
+        return true;
+    }
+}
